20a8d8443cdb1da6098ae859e607170788c40768
yujiosaka <yuji.i@doboken.net>
2016-02-14 11:24:01 +0900

    support softmax

--- dist/linear-algebra.js
+++ dist/linear-algebra.js
@@ -922,6 +922,22 @@ Matrix.prototype.eleMap_ = function(transformFn) {
 
 
 
+Matrix.prototype.softmax = function() {
+  var mat, sum, max = this.getMax();
+
+  mat = this.eleMap(function(v) { return Math.exp(v - max); });
+  sum = mat.getSum();
+  return mat.eleMap(function(v) { return v / sum; });
+};
+
+Matrix.prototype.softmax_ = function() {
+  var mat, sum, max = this.getMax();
+
+  this.eleMap_(function(v) { return Math.exp(v - max); });
+  this.getSum();
+  return this.eleMap_(function(v) { return v / sum; });
+};
+
 
 
     return LinAlg;
