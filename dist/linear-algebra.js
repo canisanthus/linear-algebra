544cbda061d4e10b5a930fd0d37c7b2e4896b015
yujiosaka <yuji.i@doboken.net>
2016-08-07 23:55:39 +0900

    support softmax by axis

--- dist/linear-algebra.js
+++ dist/linear-algebra.js
@@ -1248,20 +1248,65 @@ Matrix.prototype.getArgMin = function(axis) {
   }
 };
 
-Matrix.prototype.softmax = function() {
-  var mat, sum, max = this.getMax();
+Matrix.prototype.softmax = function(axis) {
+  var _softmax, _softmaxByRow;
+  _softmax = function(that) {
+    var max, mat, sum;
+    max = that.getMax();
+    mat = that.eleMap(function(v) { return Math.exp(v - max); });
+    sum = mat.getSum();
+    return mat.eleMap(function(v) { return v / sum; });
+  };
 
-  mat = this.eleMap(function(v) { return Math.exp(v - max); });
-  sum = mat.getSum();
-  return mat.eleMap(function(v) { return v / sum; });
+  _softmaxByRow = function(that) {
+    var row, mat,
+      thisData = that.data,
+      rows = that.rows,
+      result = new Array(rows);
+    for (row=0; row<rows; ++row) {
+      mat = new Matrix(thisData[row])
+      result[row] = _softmax(mat).toArray()[0];
+    }
+    return new Matrix(result);
+  };
+
+  if (axis === 0) {
+    return _softmaxByRow(this.trans()).trans();
+  } else if (axis === 1) {
+    return _softmaxByRow(this);
+  } else if (axis === null || axis === undefined) {
+    return _softmax(this);
+  }
 };
 
-Matrix.prototype.softmax_ = function() {
-  var mat, sum, max = this.getMax();
+Matrix.prototype.softmax_ = function(axis) {
+  var _softmax, _softmaxByRow;
+  _softmax = function(that) {
+    var max, mat, sum;
+    max = that.getMax();
+    that.eleMap_(function(v) { return Math.exp(v - max); });
+    sum = that.getSum();
+    return that.eleMap_(function(v) { return v / sum; });
+  };
 
-  this.eleMap_(function(v) { return Math.exp(v - max); });
-  this.getSum();
-  return this.eleMap_(function(v) { return v / sum; });
+  _softmaxByRow = function(that) {
+    var row, mat,
+      thisData = that.data,
+      rows = that.rows;
+    for (row=0; row<rows; ++row) {
+      mat = new Matrix(thisData[row])
+      thisData[row] = _softmax(mat).toArray()[0];
+    }
+    return that;
+  };
+
+  if (axis === 0) {
+    return _softmaxByRow(this.trans()).trans();
+  } else if (axis === 1) {
+    return _softmaxByRow(this);
+  } else if (axis === null || axis === undefined) {
+    return _softmax(this);
+  }
 };
 
 
